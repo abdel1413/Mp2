44f53206a4de645f122d8c1fa94f8a12c916f593
abdel <aboualyet63@gmail.com>
2021-02-21 17:45:31 -0500

    added some overrides to main activity

--- app/src/main/java/com/example/mp2/MainActivity.java
+++ app/src/main/java/com/example/mp2/MainActivity.java
@@ -32,12 +32,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+
         mMessageEditText = findViewById(R.id.editText_main);  //get the reference  to the editText from layout
 
         //to get reponse from seconde activity and show it on main activity screen
         mReplyHeadTextView = findViewById(R.id.text_header_reply);  //find reference to reply header
         mReplyTextView = findViewById(R.id.text_message_reply);    //find reference to reply message
     }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        Log.d(LOG_TAG,"onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"onPause()");
+    }
+
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart(");
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Log.d(LOG_TAG,"onResume");
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        Log.d(LOG_TAG,"onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        Log.d(LOG_TAG,"onDestroy()");
+    }
+
 
     public void launchSecondActivity(View view) {
 
